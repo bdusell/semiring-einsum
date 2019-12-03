9d3837b09caeb71206b39a03c9595f5795eb3eaa
Brian DuSell <bdusell@gmail.com>
2019-12-03 01:47:51 -0500

    Add unit test for Viterbi einsum.

--- tests/test_semiring_einsum.py
+++ tests/test_semiring_einsum.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 
 import numpy
@@ -105,7 +106,60 @@ class TestSemiringEinsum(unittest.TestCase):
             numpy.testing.assert_allclose(grad, expected_grad, rtol=1e-6)
 
     def test_logspace_viterbi_einsum_forward(self):
-        pass
+        args = [
+            torch.empty(size, device=self.device)
+            for size in SIZES
+        ]
+        for arg in args:
+            arg.uniform_(-10.0, 10.0, generator=self.generator)
+        expected_maxval, expected_argmax = reference_logspace_viterbi_einsum(
+            *args, self.device)
+        self.assertEqual(expected_maxval.size(), OUTPUT_SIZE)
+        self.assertEqual(expected_argmax.size(), (*OUTPUT_SIZE, 3))
+        maxval, argmax = logspace_viterbi_einsum_forward(
+            compile_equation(EQUATION_STR),
+            *args)
+        self.assertEqual(expected_maxval.size(), OUTPUT_SIZE)
+        self.assertEqual(expected_argmax.size(), (*OUTPUT_SIZE, 3))
+        numpy.testing.assert_allclose(maxval, expected_maxval)
+        self.assertTrue(torch.equal(argmax, expected_argmax))
+
+def reference_logspace_viterbi_einsum(X1, X2, X3, device):
+    Y_max = []
+    Y_argmax = []
+    for a in range(A):
+        Y_max_a = []
+        Y_argmax_a = []
+        for c in range(C):
+            Y_max_ac = []
+            Y_argmax_ac = []
+            for d in range(D):
+                maxval = torch.tensor(-math.inf, device=device)
+                argmax = None
+                for b in range(B):
+                    for e in range(E):
+                        for f in range(F):
+                            val = (
+                                X1[a, b, c, e]
+                                + X2[a, b, d, e]
+                                + X3[a, b, d, f]
+                            )
+                            if val > maxval:
+                                maxval = val
+                                argmax = torch.tensor([b, e, f], device=device)
+                Y_max_ac.append(maxval)
+                Y_argmax_ac.append(argmax)
+            Y_max_a.append(Y_max_ac)
+            Y_argmax_a.append(Y_argmax_ac)
+        Y_max.append(Y_max_a)
+        Y_argmax.append(Y_argmax_a)
+    return recursively_stack(Y_max), recursively_stack(Y_argmax)
+
+def recursively_stack(tensors):
+    if isinstance(tensors, torch.Tensor):
+        return tensors
+    else:
+        return torch.stack([recursively_stack(x) for x in tensors])
 
 if __name__ == '__main__':
     unittest.main()
