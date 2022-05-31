ac22478d690e1c4d0ff1c61e91f577920cf2e695
Brian DuSell <bdusell@gmail.com>
2022-05-31 17:16:21 -0400

    Make it possible to pass options to forward and backward functions.

--- torch_semiring_einsum/__init__.py
+++ torch_semiring_einsum/__init__.py
@@ -14,11 +14,13 @@ This combines :py:func:`real_einsum_forward` and
 :py:func:`real_einsum_backward` into one auto-differentiable function.
 """
 
-log_einsum = combine(log_einsum_forward, log_einsum_backward)
+log_einsum = combine(log_einsum_forward, log_einsum_backward,
+    backward_options=('grad_of_neg_inf',))
 r"""Differentiable version of log-space einsum.
 
 This combines :py:func:`log_einsum_forward` and
-:py:func:`log_einsum_backward` into one auto-differentiable function.
+:py:func:`log_einsum_backward` into one auto-differentiable function. It
+accepts all options to either function.
 """
 
 __all__ = [
